273e91c3203e0efd9f67eda28b6ce31c4c9d0b6b
MehmetCan Özoğlu <mehmetcanozoglu1@gmail.com>
2023-10-18 21:35:38 +0300

    konsey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Data from './Data.json'
 
 function App() {
+  let [searchterm, setSearchterm] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Kurtlar vadisi konsey</h1>
+      <div className='inputContainer'>
+        <input className='search' type='text'
+          placeholder='konsey üyesinin adını giriniz'
+          onChange={(e) => setSearchterm(e.target.value)} />
+      </div>
+      <div className='dataContainer'>
+        {Data.filter((val) => {
+          if(searchterm == ""){
+            return val;
+          }else if (val.title.toLocaleLowerCase().includes(searchterm.toLocaleLowerCase())){
+                return val;
+          }}).map((val) =>{
+          return (
+            <div className='data' key={val.id}>
+              <img src={val.image}></img>
+              <h2>{val.title}</h2>
+              <p>{val.Description}</p>
+            </div>
+          )})}
+      </div>
     </div>
-  );
-}
+  )}
 
 export default App;
